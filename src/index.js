3a15cbd805ebaf2390f6dcfc35da4a428beec41c
Jonathas <jonathass5678@gmail.com>
2023-03-26 13:51:09 -0300

    serverpedidos

--- src/index.js
+++ src/index.js
@@ -5,20 +5,18 @@ import pedidos from './models/pedidos.js'
 const app = express();
 app.use(express.json());
 
-app.get("/pedidos", async (req, res) => {
+app.get("/pedidos", async (req, res)=>{
   const pedido = await pedidos.find()
   return res.status(200).json(pedido)
 })
 
-app.post("/pushpedidos", async (req, res) => {
+app.post("/pushpedidos", async (req, res)=>{
   const pedido = req.body
   const newPedido = await pedidos.create(pedido)
 
   return res.json(newPedido);
 });
 
-connectDatabase().then(() => {
-  app.listen(3000, ()=>console.log("Servidor rodando e banco conectado!"))
-}).catch((error) => {
-  console.log(error)
-})
+connectDatabase().then(()=>{console.log("bd connected")}).catch(()=>{console.log("Deu ruin")})
+
+app.listen(3000)
